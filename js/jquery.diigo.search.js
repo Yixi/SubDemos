d78befab3664371b291a5138da91be2c9d480599
Yixi <lyh1112@gmail.com>
2013-03-29 16:09:36 +0800

    user stylesheet

--- js/jquery.diigo.search.js
+++ js/jquery.diigo.search.js
@@ -32,10 +32,13 @@
             "type":"Library",   //Library(advance search panel)  | Global  | null(only auto complete)
             //data
             "data":[],
-            "maxItems":20,
+            "maxItems":10,
             "minLength":1,
             "mutil":false,
-            "delay":300
+            "delay":300,
+
+            //complete
+            "complete":null
 
         },option);
 
@@ -81,6 +84,13 @@
                     case 13: //enter
                         //TODO:begin to search
 
+                        var json = {
+                            "type": that.typeView && that.typeView.attr('id').replace('dls_type_',""),
+                            "value":that.inputView.val()
+                        };
+                        if($.isFunction(that.option.complete)){
+                            that.option.complete(json);
+                        }
                         break;
                 }
             })
@@ -98,21 +108,23 @@
     var _setupLibrarySearchPanel = function(){
         var that = this;
 
-        this.inputView.wrap('<span class="diigols_box"></span>').css('border','none');
+        this.inputView.wrap('<span class="diigols_box"></span>').css('float','left').css('border','none');
         var _h = this.inputView.outerHeight()
         this.SearchPanel = this.inputView.parent().css('position','relative');
-        this.typeView = $('<span class="dls_type">tag</span>').css('height',_h).css('line-height',_h+"px");
-        this.searchIcon = $('<span class="dls_icon">Seach</span> ').css('height',_h).css('line-height',_h+"px");
+        this.typeView = $('<span class="dls_type"><span>&nbsp;</span></span>').css('height',_h).css('line-height',_h+"px");
+        this.advanceIcon= $('<span class="dls_adv"><b>&nbsp;</b></span> ').css('height',_h).css('line-height',_h+"px");
         this.SearchPanel.prepend(that.typeView);
-        this.SearchPanel.append(that.searchIcon);
+        this.SearchPanel.append(that.advanceIcon);
 
-        this.typeView.attr('id','dls_tag');  //default search tag.
+        this.typeView.attr('id','dls_type_tag');  //default search tag.
+
+        _setupAdvancePanel.apply(this);
         _setupTypeSelectView.apply(this);
     }
 
     var _setupTypeSelectView = function(){
         var that = this;
-        this.TypeSelectView = $("<div class='dls_list dls_type_list'><ul><li>Tag Search</li><li>Meta Search</li><li>Full text Search</li></ul></div> ")
+        this.TypeSelectView = $("<div class='dls_list dls_type_list'><ul><li val='tag'><span class='dls_tag_icon'>&nbsp;</span>Tag Search</li><li val='meta'><span class='dls_meta_icon'>&nbsp;</span>Meta Search</li><li val='full'><span class='dls_full_icon'>&nbsp;</span>Full text Search</li></ul></div> ")
             .appendTo(document.body)
             .on('mouseenter','li',function(){
                 that.TypeSelectView.find('li.selected').removeClass("selected");
@@ -122,7 +134,21 @@
                 $(this).removeClass('selected');
             })
             .on('click','li',function(e){
-                console.log(e);
+                var type = $(this).attr('val');
+                switch(type){
+                    case 'tag':
+                        that.typeView.attr('id','dls_type_tag');
+                        _fillAdvancePanel.apply(that,['tag']);
+                        break;
+                    case 'meta':
+                        that.typeView.attr('id','dls_type_meta');
+                        _fillAdvancePanel.apply(that);
+                        break;
+                    case 'full':
+                        that.typeView.attr('id','dls_type_full');
+                        _fillAdvancePanel.apply(that);
+                        break;
+                }
             })
             .css('font-size',this.typeView.css('font-size'))
             .hide();
@@ -142,7 +168,55 @@
             });
         var top = this.typeView.offset().top + this.typeView.outerHeight();
         var left =this.typeView.offset().left;
-        this.TypeSelectView.css("top",top+1+"px").css("left",left-1+"px").css('position',"absolute");
+        this.TypeSelectView.css("top",top+"px").css("left",left-1+"px").css('position',"absolute");
+    }
+
+    var _setupAdvancePanel = function(){
+        var that =this;
+        this.adVancePanel = $('<div class="dls_advpanel dls_list"></div>').appendTo(document.body).hide();
+
+        this.advanceIcon
+            .click(function(e){
+//                if(that.adVancePanel.is(':visible')){
+//                    that.adVancePanel.hide();
+//                }else{
+////                    that.adVancePanel.show(10,function(){
+////                        $(document).one('click',function(){
+////                           that.adVancePanel.hide();
+////                        });
+////                    });
+//                    that.adVancePanel.show();
+//                }
+                that.adVancePanel.toggle();
+                $(this).toggleClass('dls_adv_show');
+            });
+
+        var top = this.SearchPanel.offset().top + this.SearchPanel.outerHeight();
+        var left = this.SearchPanel.offset().left;
+        this.adVancePanel.css("top",top-1+"px").css("left",left-1+"px").css('position',"absolute").css('width',_calcWidth.apply(this));
+        _fillAdvancePanel.apply(this,['tag']);
+    }
+
+    var _fillAdvancePanel = function(type){
+        var m_f = '<div class="dls_advpanel_title">Advanced Search</div>' +
+            '<div><p>Tagged</p><p><input type="text" name="tag"/></p></div>' +
+            '<div><p>Full text</p><p><input type="text" name="fulltext"/></p></div>' +
+            '<div><p>URL</p><p><input type="text" name="URL"/></p></div>' +
+            '<div><p>Title</p><p><input type="text" name="title"/></p></div>' +
+            '<div><p>Description</p><p><input type="text" name="description00"/></p></div>' +
+            '<div><p>Highlights</p><p><input type="text" name="highlights"/></p></div>' +
+            '<div><p>Without the words</p><p><input type="text" name="without"/></p></div>' +
+            '<div class="dls_advpanel_search"><a href="javascript:void(0)" class="dls_search"></a></div>';
+        var tagpanel = '<div class="dls_advpanel_title">Advanced Search</div>' +
+            '<div><p>AND</p><p><input type="text" name="tagAND"/></p></div>' +
+            '<div><p>OR</p><p><input type="text" name="tagOR" /></p></div>' +
+            '<div><p>NOT</p><p><input type="text" name="tagNOT" /></p></div>' +
+            '<div class="dls_advpanel_search"><a href="javascript:void(0)" class="dls_search"></a></div>';
+        if(type=='tag'){
+            this.adVancePanel.html(tagpanel).removeClass('dls_meta').addClass('dls_tag');
+        }else{
+            this.adVancePanel.html(m_f).removeClass('dls_tag').addClass('dls_meta');
+        }
     }
 
     var _setupSuggestionView = function(){
@@ -173,9 +247,12 @@
         if(selected.length>0){
             var nextSelected = dir ==='up'? selected.prev() : selected.next();
         }else{
-            var nextSelected = dir ==='up'? this.SuggestionView.find('li').last() : this.SuggestionView.find('li').first();
+            var nextSelected = dir ==='up'? this.SuggestionView.find('li').last('li') : this.SuggestionView.find('li').first('li');
         }
         if(nextSelected.length>0){
+            if(nextSelected[0].tagName.toLowerCase()=='hr'){
+                var nextSelected = dir ==='up'? nextSelected.prev() : nextSelected.next();
+            }
             this.SuggestionView.find('li').removeClass("selected");
             nextSelected.addClass("selected");
         }
@@ -185,20 +262,67 @@
     var _createItems = function(result){
         var that = this,
             container = this.SuggestionView.find('ul').empty();
+        if($.isNumeric(that.option.maxItems)) result = result.slice(0,that.option.maxItems);
+        else result = result.slice(0,10);
+        var html="",
+            tags="",
+            meta="",
+            full="";
 
+        //TODO: maybe need to support the option mutil value spliter;
+        var words = this.inputView.val().split(" ");
+        var lastwords = words[words.length-1];
+        delete words[words.length-1];
+        words = words.join(" ");
+
+        if(!!this.option.mutil===false) {
+            lastwords = this.inputView.val();
+            words="";
+        }
+
+        meta +='<li val="'+lastwords+'"><div><span class="dls_meta_icon"></span>'+((that.typeView.attr('id')=="dls_type_meta")?'':'Meta:')+'<span>'+words+" "+lastwords+'</span></div></li> ';
+        full +='<li val="'+lastwords+'"><div><span class="dls_full_icon"></span>'+((that.typeView.attr('id')=="dls_type_full")?'':'Full Text:')+'<span>'+words+" "+lastwords+'</span></div></li> ';
+        tags +='<li val="'+lastwords+'"><div><span class="dls_tag_icon"></span>'+((that.typeView.attr('id')=="dls_type_tag")?'':'Tag:')+'<span>'+words+" "+lastwords+'</span></div></li> ';
         $.each(result,function(index,data){
-            var item = $("<li><div></div></li>").appendTo(container).attr("val",data).find("div");
-            item.append("<span>"+data+"</span>");
+//            var item = $("<li><div></div></li>").appendTo(container).attr("val",data).find("div");
+//            item.append("<span>"+data+"</span>");
+//
+            if(that.typeView.attr('id')=="dls_type_tag")
+                tags += '<li val="'+data+'"><div><span class="dls_tag_icon"></span><span>'+data+'</span></div></li>';
+            if(that.typeView.attr('id')=="dls_type_meta")
+                meta += '<li val="'+data+'"><div><span class="dls_meta_icon"></span><span>'+words+" "+data+'</span></div></li>';
+            if(that.typeView.attr('id')=="dls_type_full")
+                full += '<li val="'+data+'"><div><span class="dls_full_icon"></span><span>'+words+" "+data+'</span></div></li> ';
 
-            if(that.option.itemHeight > 0 ){
-                item.height(that.option.itemHeight).css('max-height',that.option.itemHeight);
-            }
         });
+        if(this.option.type==="Library"){
+
+            if(this.typeView.attr('id')=='dls_type_meta'){
+                html = meta+'<hr />'+tags+full;
+            }else if(this.typeView.attr('id')=='dls_type_full'){
+                html = full+'<hr />'+tags+meta;
+            }else{
+                if(tags.length<1){
+                    html = meta+full;
+                }else{
+                    html = tags+'<hr />'+meta+full;
+                }
+            }
+            $(html).appendTo(container);
+
+        }else{
+            $(tags).appendTo(container);
+        }
+
 
     }
     var _calcWidth = function(){
         if(typeof(this.option.width)==='string' && this.option.width.toLowerCase()==='auto'){
-            return this.inputView.outerWidth() -2;
+            if(this.option.type === 'Library'){
+                return this.SearchPanel.outerWidth() -2;
+            }else{
+                return this.inputView.outerWidth() -2;
+            }
         }else if(typeof(this.option.width)==='number'){
             return this.option.width;
         }
@@ -207,9 +331,14 @@
 
     var _locateSuggestionView = function(){
         //定位补全列表
-        var top = this.inputView.offset().top + this.inputView.outerHeight();
-        var left =this.inputView.offset().left;
-        this.SuggestionView.css("top",top+"px").css("left",left+"px").css('position',"absolute");
+        if(this.option.type ==='Library'){
+            var top = this.SearchPanel.offset().top + this.SearchPanel.outerHeight();
+            var left = this.SearchPanel.offset().left;
+        }else{
+            var top = this.inputView.offset().top + this.inputView.outerHeight();
+            var left =this.inputView.offset().left;
+        }
+        this.SuggestionView.css("top",top-1+"px").css("left",left+"px").css('position',"absolute");
 
     }
 
